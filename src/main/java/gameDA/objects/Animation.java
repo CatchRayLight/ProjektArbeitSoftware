da412571b7b9650a5ad31c7f241735f0d8f13ef3
David <Dawitschi008@gmail.com>
2022-06-23 18:21:51 +0200

    comments

--- src/main/java/gameDA/objects/Animation.java
+++ src/main/java/gameDA/objects/Animation.java
@@ -4,32 +4,42 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Animation {
-    private int speed;
-    private final int frames;
-    private int index = 0;
+
+    private int speed; //Geschwindigkeit der Animation in frames pro Bild
+    private final int frames; //Anzahl an frames für die Animation
+    private int index = 0; //Anzahl an vergangenen Frames
     private int count = 0;
 
-    private final BufferedImage[] img = new BufferedImage[14];
-    private BufferedImage currentImg;
+    private final BufferedImage[] img = new BufferedImage[14]; //Bilder der Animation
+    private BufferedImage currentImg;//momentanes Image
 
     public Animation(int speed, BufferedImage[] img){
         this.speed = speed;
         System.arraycopy(img, 0, this.img, 0, img.length);
         frames = img.length;
     }
+
+    /**
+     * Führt die Animation aus
+     */
     public void runAnimation(){
+        //wechselt den Frame zyklisch je nach speed der Animation
         index++;
         if(index > speed){
             index = 0;
             nextFrame();
         }
     }
+
+    /**
+     * Sets the current Frame to the next Frame based on speed
+     */
     public void nextFrame(){
+        //Basierend auf Speed wird der nächst frame zum momentanen Frame gestzt
         for(int i = 2; i < img.length; i++) {
             if(frames == i) {
                 for(int j = 0; j < i; j++) {
-                    if(count == j)
-                        currentImg = img[j];
+                    if(count == j) currentImg = img[j];
                 }
                 count++;
                 if(count > frames)
@@ -39,20 +49,14 @@ public class Animation {
         }
     }
 
+    /**
+     * Zeichnet das momentane Image der Animation
+     * @param g Die Grafik mit der gerendert werdeb soll
+     * @param x X Position
+     * @param y Y Position
+     * @param offset Das Offset
+     */
     public void drawAnimation(Graphics g, double x, double y, int offset){
         g.drawImage(currentImg, (int)x - offset, (int)y, null);
-    }
-
-    public void setCount(int count){
-        this.count = count;
-    }
-    public int getCount(){
-        return count;
-    }
-    public int getSpeed(){
-        return speed;
-    }
-    public void setSpeed(int speed){
-        this.speed = speed;
     }
 }
